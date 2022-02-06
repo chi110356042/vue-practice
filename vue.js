31cadf0dded8850697ad41126c9786e8701fcbfc
chi110356042 <92499570+chi110356042@users.noreply.github.com>
2022-02-06 23:22:36 +0800

    Update vue.js

--- vue.js
+++ vue.js
@@ -5,16 +5,16 @@ export default {
   data() {
     return {
       seen: "",
-      unseenImg: require("eye.png"),   //看不见密码时小眼睛的显示图片地址
-      seenImg: require("eye.png"),   //看得见密码时小眼睛的显示图片地址
+      unseenImg: require("eye.png"),   
+      seenImg: require("eye.png"),   
       eyeTxt: "",
-      pwdType: false   //此时文本框隐藏，显示密码框 
+      pwdType: false  
     };
   },
   methods: {
   	changeType: function() {
-     	this.seen = !this.seen;   //小眼睛的变化
-     	this.pwdType = !this.pwdType;   //跟着小眼睛变化，密码框隐藏，显示文本框 内容就显示了
+     	this.seen = !this.seen;  
+     	this.pwdType = !this.pwdType;   
     },
     hoverEye: function(event) {
     	this.seen = !this.seen;
